d3bfc7e9264f635454b3125d8481531bee06eeb0
Woo Sunmyung (禹 善明) <woo_su@micware.co.jp>
2026-04-14 10:43:51 +0900

    welcome screen on first load

---
+++ app/src/main/java/com/example/noteapp/ui/notes/WelcomeFragment.kt
@@ -0,0 +1,33 @@
+package com.example.noteapp.ui.notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.noteapp.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.setOnClickListener {
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToNoteListFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
+
+
+
+
